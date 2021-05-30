2e374d359b1cfea80f449aacca73aafce836aedb
Adam Romeo <adam.r.romeo@gmail.com>
2021-05-30 16:09:16 -0700

    Setup server

---
+++ multer-server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5000'
+  })
+);
+
+app.post('/upload', upload.single('media'), function (req, res, next) {
+  console.log('req.file', req.file);
+  res.send();
+});
+
+app.listen('4000', () => {
+  console.log('Listening on port 4000...');
+});
